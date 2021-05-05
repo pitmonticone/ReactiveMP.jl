81feb542172c6f3823e6c5c5984183c1a4bd7e63
ismail <i.senoz@tue.nl>
2021-05-05 23:18:40 +0300

    wip sgcv

---
+++ src/distributions/exp_linear_quadratic_mv_u.jl
@@ -0,0 +1,30 @@
+export MvUniExponentialLinearQuadratic
+
+import Distributions: pdf, logpdf, ContinuousUnivariateDistribution
+
+struct MvUniExponentialLinearQuadratic{ A <: AbstractApproximationMethod, T <: Real , M <: AbstractVector{T}} <: ContinuousUnivariateDistribution
+    approximation :: A
+    a :: T
+    b :: T
+    c :: M
+    d :: M
+    e :: M
+    f :: M
+end
+
+function MvUniExponentialLinearQuadratic(approximation, a::Real, b::Real, c::AbstractVector{ <: Real}, d::AbstractVector{ <: Real},e::AbstractVector{ <: Real},f::::AbstractVector{ <: Real})          
+    T = promote_type(eltype(c),eltype(d), eltype(e),eltype(f))
+    MvUniExponentialLinearQuadratic(approximation, promote(a),promote(b),convert(AbstractArray{T},c),convert(AbstractArray{T},d),convert(AbstractArray{T},e),convert(AbstractArray{T},f))
+end
+
+
+Distributions.pdf(dist::MvUniExponentialLinearQuadratic, x::Real)    = exp(logpdf(dist, x))
+Distributions.logpdf(dist::MvUniExponentialLinearQuadratic, x::Real) = -0.5 * (dist.a * x + dist.b + dist.c' * exp.(dist.d .* x + dist.e + 0.5 .* dist.f .* x^2 ))
+Distributions.mean(dist::MvUniExponentialLinearQuadratic)            = approximate_meancov(dist.approximation, (z) -> pdf(dist, z) * exp(0.5 * z^2), NormalMeanVariance(0.0,1.0))[1]
+Distributions.var(dist::MvUniExponentialLinearQuadratic)             = approximate_meancov(dist.approximation, (z) -> pdf(dist, z) * exp(0.5 * z^2), NormalMeanVariance(0.0,1.0))[2]
+Distributions.cov(dist::MvUniExponentialLinearQuadratic)             = var(dist)
+
+function prod(::ProdPreserveParametrisation, left::UnivariateNormalDistributionsFamily, right::MvUniExponentialLinearQuadratic)
+    mean, variance = approximate_meancov(right.approximation, (z) -> pdf(right, z), left)
+    return NormalMeanVariance(mean, variance)
+end
